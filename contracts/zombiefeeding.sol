5255757f26ea4c3e6dd3273c7f448d424427a9d4
zslxbl <zslxbl@163.com>
2018-09-03 00:16:42 +0800

    update

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -19,22 +19,31 @@ contract KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
-    // 1. 移除这一行:
-    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-    // 2. 只声明变量:
-    KittyInterface kittyContract = KittyInterface(ckAddress);
+    KittyInterface kittyContract;
 
-    // 3. 增加 setKittyContractAddress 方法
+    function setKittyContractAddress(address _address) external onlyOwner {
+        kittyContract = KittyInterface(_address);
+    }
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string species) public {
+    function _triggerCooldown(Zombie storage _zombie) internal {
+        _zombie.readyTime = uint32(now + cooldownTime);
+    }
+
+    function _isReady(Zombie storage _zombie) internal view returns (bool) {
+        return (_zombie.readyTime <= now);
+    }
+
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
+        require(_isReady(myZombie));
         _targetDna = _targetDna % dnaModulus;
         uint newDna = (myZombie.dna + _targetDna) / 2;
-        if (keccak256(species) == keccak256("kitty")) {
+        if (keccak256(_species) == keccak256("kitty")) {
             newDna = newDna - newDna % 100 + 99;
         }
         _createZombie("NoName", newDna);
+        _triggerCooldown(myZombie);
     }
 
     function feedOnKitty(uint _zombieId, uint _kittyId) public {
@@ -42,5 +51,4 @@ contract ZombieFeeding is ZombieFactory {
         (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
         feedAndMultiply(_zombieId, kittyDna, "kitty");
     }
-
 }
